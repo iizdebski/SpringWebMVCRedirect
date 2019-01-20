2e1ec8aef36b76f9b1e4a8d076b20944d73b8453
Ivan Izdepskyi <iizdepskyy112@gmail.com>
2019-01-20 19:41:40 +0200

    Spring MVC Page Redirection Example | Spring Web MVC redirect

---
+++ out/artifacts/RedirectWebApp_war_exploded/WEB-INF/src/com/izdebski/dao/UserDAO.java
@@ -0,0 +1,24 @@
+package com.izdebski.dao;
+
+import com.izdebski.model.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UserDAO {
+    private static List<User> list = null;
+    static{
+        list=new ArrayList<>();
+        list.add(new User("Robert", 30, "rt.cs@gmail.com", "India"));
+        list.add(new User("Martin", 40, "martin.cs@gmail.com", "US"));
+        list.add(new User("Luther", 30, "sean.cs@gmail.com", "Canada"));
+    }
+    public List<User>getUserList(){
+        return list;
+    }
+    public void addUser(User user){
+        list.add(user);
+    }
+}
